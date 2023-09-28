dce8e4c4694fb9cfd87fddc6535d43e446d36b0b
tomneozed <tomneozed@gmail.com>
2023-09-28 19:00:06 +0200

    Fix conflicts + code refactor

--- src/app/favourite.service.ts
+++ src/app/favourite.service.ts
@@ -1,31 +1,56 @@
-import { Injectable } from '@angular/core';
-import { FavouriteItem } from './favourite-item';
-import { CategoryItem } from './category-item';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { FavouriteItem } from "./favourite-item";
+import { CategoryItem } from "./category-item";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class FavouriteService {
-  async getAllFavourites(): Promise<FavouriteItem[]> {
-    const data = fetch('https://localhost:7280/api/favorite/get');
-    return (await data).json() ?? [];
-  }
-  async getAllCategories(): Promise<CategoryItem[]>{
-    const data = fetch('https://localhost:7280/api/category/get');
-    return (await data).json() ?? [];
-    
-  }
+  constructor(private http: HttpClient) {}
+
+  /* FAVOURITES */
+
   addFavourite(favourite: FavouriteItem): void {
     const copyOfFavourite: FavouriteItem = { ...favourite };
-    fetch('https://localhost:7280/api/favourite/add', {method: "POST", body: JSON.stringify(copyOfFavourite)});
+    fetch("https://localhost:7280/api/favourite/add", {
+      method: "POST",
+      body: JSON.stringify(copyOfFavourite),
+    });
   }
-  async filterFavourites(categoryId: number): Promise<FavouriteItem[]>{
-  const data = fetch (`https://localhost:7280/api/favorite/filter/`+categoryId);
-  return (await data).json() ?? [];
-}
 
-  constructor(
-    private http: HttpClient
-  ){}
+  async getAllFavourites(): Promise<FavouriteItem[]> {
+    const data = fetch("https://localhost:7280/api/favorite/get");
+    return (await data).json() ?? [];
+  }
+
+  async deleteFavorite(ids: number[]): Promise<void> {
+    const url = "https://localhost:7280/api/favorite/delete";
+
+    const response = await fetch(url, {
+      method: "DELETE",
+      body: JSON.stringify(ids),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to delete favorites: ${response.statusText}`);
+    }
+  }
+
+  async filterFavourites(categoryId: number): Promise<FavouriteItem[]> {
+    const data = fetch(
+      `https://localhost:7280/api/favorite/filter/` + categoryId
+    );
+    return (await data).json() ?? [];
+  }
+
+  /* CATEGORIES */
+
+  async getAllCategories(): Promise<CategoryItem[]> {
+    const data = fetch("https://localhost:7280/api/category/get");
+    return (await data).json() ?? [];
+  }
 }
